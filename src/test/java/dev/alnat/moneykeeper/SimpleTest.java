6e58a31aaea2245bdb68698289e4406f0067bb8c
AlNat <aleksnatarov@gmail.com>
2020-07-19 21:45:13 +0300

    Создание первичного проекта
    Добавлен Maven и SpringBoot

---
+++ src/test/java/dev/alnat/moneykeeper/SimpleTest.java
@@ -0,0 +1,19 @@
+package dev.alnat.moneykeeper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by @author AlNat on 19.07.2020.
+ * Licensed by Apache License, Version 2.0
+ */
+public class SimpleTest {
+
+    @Test
+    public void simpleTestForExample() {
+        int a = 1;
+        int b = 1;
+        Assertions.assertEquals(a, b);
+    }
+
+}
